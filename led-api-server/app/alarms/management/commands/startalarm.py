3eb072f68abd6e02fcdbc8f70dab7f147429c218
Harm van Zeeland <harm.van.zeeland@gmail.com>
2020-10-06 16:47:19 +0200

    Improve logging of alarm management command

--- led-api-server/app/alarms/management/commands/startalarm.py
+++ led-api-server/app/alarms/management/commands/startalarm.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.core.management.base import BaseCommand, CommandError
 from api.zmq_client import ZMQClient
 
@@ -5,12 +6,17 @@ from api.zmq_client import ZMQClient
 class Command(BaseCommand):
     help = "Triggers alarm scripts on zmq server"
 
+    def log(self, msg):
+        dt = datetime.now()
+        self.stdout.write("{} - {}".format(dt, msg))
+
     def handle(self, *args, **options):
+
+        self.log("Starting alarm")
         try:
             zmqc = ZMQClient()
             zmqc.perform_request("start_alarm")
         except Exception as e:
+            self.log(str(e))
             raise CommandError(str(e))
-
-        self.stdout.write(self.style.SUCCESS("Successfully started"))
-
+        self.log("Alarm successfully started")
