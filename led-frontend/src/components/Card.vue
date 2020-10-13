1c317bc49b329b9321b1a602ba2ddec7391e28be
Harm van Zeeland <harm.van.zeeland@gmail.com>
2020-10-13 16:34:51 +0200

    Add Card functional component

---
+++ led-frontend/src/components/Card.vue
@@ -0,0 +1,27 @@
+<template functional>
+  <q-card
+    class="col-12 bg-grey-10 text-center q-mt-md"
+    dark
+  >
+    <q-card-section
+      v-if="props.title !== ''"
+      class="bg-grey-9 text-grey-4"
+    >
+      <div class="text-h5">
+        {{ props.title }}
+      </div>
+    </q-card-section>
+    <slot />
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
