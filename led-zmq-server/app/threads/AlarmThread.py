26cf38b15a1c53a4c3f016a5eb613c8d1ed150c7
Harm van Zeeland <harm.van.zeeland@gmail.com>
2020-10-19 18:23:15 +0200

    Improve alarm functionality on zmq server and add mpd integration

--- led-zmq-server/app/threads/AlarmThread.py
+++ led-zmq-server/app/threads/AlarmThread.py
@@ -1,27 +1,32 @@
 import time
 
-# import pygame
 import numpy as np
-
-# from gtts import gTTS
+from mopidy_json_client import MopidyClient
 from rpi_ws281x import Color
-from settings import LED_DITHERING_STEPS
 
-# from utils.alarm_text import create_text
 from utils.stoppable_thread import StoppableThread
-from mpd import MPDClient
 
 
 class AlarmThread(StoppableThread):
     def __init__(self, strip, **kwargs):
         super().__init__(strip)
+
+        self.timestep = 0.2  # duration of each step in seconds
+        self.mopidy = False
+        self.volume = 0
+
         self.duration = kwargs["duration"]
         self.colors = kwargs["colors"]
         self.playlist = kwargs["playlist"]
         self.volumes = kwargs["volumes"]
 
-        self.timestep = 0.2  # duration of each step in seconds
+        self.n_pixels = self.strip.numPixels()
+        self.start_time = time.time()
         self.steps = self.duration / self.timestep  # nr of steps
+        self.nr_of_color_transitions = len(self.colors) - 1
+        self.steps_per_color = self.steps / self.nr_of_color_transitions
+        self.start_volume = self.volumes[0][1]
+        self.end_volume = self.volumes[-1][1]
 
     def run(self):
         """
@@ -30,37 +35,100 @@ class AlarmThread(StoppableThread):
         :param timestep: time that one step takes in ms
         """
 
-        def transition(start_color, final_color, steps):
-            """ Transition with dithering """
-            color_delta = final_color - start_color
-            for i in range(steps):
-                # create linear i in range 0 to 100
-                lin_range = i / (steps - 1) * 100
-                color_rgb = start_color + color_delta * lin_range / 100
-                color = Color(int(color_rgb[0]), int(color_rgb[1]), int(color_rgb[2]))
-                for i in range(4):
-                    for j in range(0, self.strip.numPixels(), LED_DITHERING_STEPS):
-                        self.strip.setPixelColor(j + i, color)
-                    self.strip.show()
+        # def transition(start_color, final_color, steps):
+        #     """ Transition with dithering """
+        #     color_delta = final_color - start_color
+        #     for i in range(steps):
+        #         # create linear i in range 0 to 100
+        #         lin_range = i / (steps - 1) * 100
+        #         color_rgb = start_color + color_delta * lin_range / 100
+        #         color = Color(int(color_rgb[0]), int(color_rgb[1]), int(color_rgb[2]))
+        #         for i in range(4):
+        #             for j in range(0, self.strip.numPixels(), LED_DITHERING_STEPS):
+        #                 self.strip.setPixelColor(j + i, color)
+        #             self.strip.show()
 
-                    if self.stopped():
-                        return False
-                    time.sleep(self.timestep / LED_DITHERING_STEPS)
-            return True
+        #             if self.stopped():
+        #                 return False
+        #             time.sleep(self.timestep / LED_DITHERING_STEPS)
+        #     return True
 
-        start_time = time.time()
-        nr_of_color_transitions = len(self.colors) - 1
+        # for color_idx in range(nr_of_color_transitions):
+        #     start_color = np.array(self.colors[color_idx])
+        #     finish_color = np.array(self.colors[color_idx + 1])
+        #     if not transition(
+        #         start_color, finish_color, int(self.steps / nr_of_color_transitions)
+        #     ):
+        #         return
 
-        # TODO start music, progress volume
-        # client = MPDClient(use_unicode=True)
-        # client.connect('localhost', 6690) # TODO
-        # for entry in client.lsinfo("/"):
-        #     pass
+        def on_mopidy_connected(connection_status):
+            print("connecting mopidy triggered", connection_status)
 
-        for color_idx in range(nr_of_color_transitions):
-            start_color = np.array(self.colors[color_idx])
-            finish_color = np.array(self.colors[color_idx + 1])
-            if not transition(
-                start_color, finish_color, int(self.steps / nr_of_color_transitions)
-            ):
-                return
+            if connection_status:
+                print("starting mopidy requests")
+
+                playlist = self.mopidy.playlists.lookup(self.playlist)
+                uris = [t["uri"] for t in playlist["tracks"]]
+                self.mopidy.playback.stop()
+                self.mopidy.tracklist.clear()
+                self.mopidy.tracklist.add(uris=uris)
+                self.mopidy.mixer.set_volume(self.start_volume)
+                self.volume = self.start_volume
+                self.mopidy.playback.play()
+                print(" mopidy requests play")
+
+        def step_color(step_nr):
+            color_from = np.array(self.colors[int(step_nr / self.steps_per_color)])
+            color_to = np.array(self.colors[int(step_nr / self.steps_per_color) + 1])
+            color_delta = color_to - color_from
+
+            color_rgb = (
+                color_from
+                + ((step_nr % self.steps_per_color) / self.steps_per_color)
+                * color_delta
+            )
+            color = Color(*[int(c) for c in color_rgb])
+            # TODO dithering
+            for i in range(self.n_pixels):
+                self.strip.setPixelColor(i, color)
+            self.strip.show()
+
+        def step_volume(step_nr):
+            step_ratio = step_nr / self.steps
+            volume_delta = self.end_volume - self.start_volume
+            volume = int(self.start_volume + step_ratio * volume_delta)
+            if self.volume != volume:
+                self.mopidy.mixer.set_volume(volume)
+                self.volume = volume
+
+        print("starting alarmthread")
+        if self.playlist != "":
+            self.mopidy = MopidyClient(
+                ws_url="ws://raspberrypi:6680/mopidy/ws",
+                error_handler=lambda e: print("ws error", e),
+                connection_handler=on_mopidy_connected,
+                autoconnect=False,
+                retry_max=10,
+                retry_secs=10,
+            )
+            print("connecting mopidy")
+
+            self.mopidy.connect()
+            print("connecting mopidy 1")
+
+        step = 0
+        while step < self.steps:
+            step_color(step)
+            if self.mopidy:
+                step_volume(step)
+
+            time.sleep(self.timestep)
+
+            expected_time = self.start_time + step * self.timestep
+            actual_time = time.time()
+            time_delta = actual_time - expected_time
+            step_correction = time_delta / self.timestep
+            step += step_correction + 1
+
+            if self.stopped():
+                return False
